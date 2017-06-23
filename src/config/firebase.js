dd2f00f3c0c464d1eb8e48cb06af4549b047dd6d
PNNutkung <pipatpol.t@ku.th>
2017-06-23 23:10:36 +0900

    Fix single quote

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase'
 
 const config = {
-  apiKey: "AIzaSyCF5QtGXowzKdVZCXitBuG-TaxfIGcRH7g",
-  authDomain: "sud-mun-pwa-2017.firebaseapp.com",
-  databaseURL: "https://sud-mun-pwa-2017.firebaseio.com",
-  projectId: "sud-mun-pwa-2017",
-  storageBucket: "sud-mun-pwa-2017.appspot.com",
-  messagingSenderId: "701725827130"
+  apiKey: 'AIzaSyCF5QtGXowzKdVZCXitBuG-TaxfIGcRH7g',
+  authDomain: 'sud-mun-pwa-2017.firebaseapp.com',
+  databaseURL: 'https://sud-mun-pwa-2017.firebaseio.com',
+  projectId: 'sud-mun-pwa-2017',
+  storageBucket: 'sud-mun-pwa-2017.appspot.com',
+  messagingSenderId: '701725827130'
 }
 
 firebase.initializeApp(config)
