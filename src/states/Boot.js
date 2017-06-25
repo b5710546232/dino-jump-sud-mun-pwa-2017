0c49927f75ede8c392e72ce7d2eaccbddd7c435f
PNNutkung <pipatpol.t@ku.th>
2017-06-25 09:34:35 +0900

    Change firebase setup

--- src/states/Boot.js
+++ src/states/Boot.js
@@ -1,9 +1,6 @@
 import Phaser from 'phaser'
-import * as firebase from 'firebase'
-import config from '../config'
-   // Initialize Firebase
+import { firebaseAuth, db } from '../config/firebase'
 
-const firebaseDB = firebase.initializeApp(config.firebaseConfig)
 export default class extends Phaser.State {
   init () {
     this.stage.backgroundColor = '#57546f'
@@ -13,21 +10,19 @@ export default class extends Phaser.State {
     this.scale.pageAlignVertically = true
     this.game.scale.setScreenSize = true
     this.game.scale.refresh()
-    // this.scale.refresh();
 
-    this.game.firebase = firebaseDB.database(); //  eslint-disable-line
+    this.game.firebase = db
 
-    firebaseDB.auth().onAuthStateChanged((user) => {
+    firebaseAuth().onAuthStateChanged((user) => {
       if (user) {
-          // User is signed in.
         let isAnonymous = user.isAnonymous
         let uid = user.uid
         console.log('user-id', uid, isAnonymous)
 
-        this.game.current_user = firebaseDB.auth().currentUser
+        this.game.current_user = firebaseAuth().currentUser
         console.log('current-user', this.game.current_user)
       } else {
-        firebaseDB.auth().signInAnonymously().catch(function (error) {
+        firebaseAuth().signInAnonymously().catch(function (error) {
           var errorCode = error.code
           var errorMessage = error.message
           console.log(errorMessage)
@@ -39,43 +34,9 @@ export default class extends Phaser.State {
         })
       }
     })
-    // firebaseDB.database().onDisconnect().set('I disconnected!')
-    // if (firebaseDB.database().onDisconnect()) {
-    //   // this.game.firebase = firebaseDB.database()
-    //   this.game.disconnect = true
-    //   console.log('disconnect')
-    // }
-    // firebase.auth().onAuthStateChanged((user) => {  //  eslint-disable-line
-    //   if (user) {
-    //             // User is signed in.
-    //     // let isAnonymous = user.isAnonymous;
-    //     // let uid = user.uid;
-    //     // console.log('user-id', uid)
-    //     // this.uid = uid;
-    //     // this.game.current_user = firebase.auth().currentUser;
-    //         // console.log('current-user', this.game.current_user)
-    //   } else {
-    //     firebase.auth().signInAnonymously().catch(function (error) { //eslint-disable-line
-    //       var errorCode = error.code
-    //       // var errorMessage = error.message
-    //       if (errorCode === 'auth/operation-not-allowed') {
-    //         alert('You must enable Anonymous auth in the Firebase Console.');
-    //       } else {
-    //         console.error(error)
-    //       }
-    //     })
-    //   }
-    // })
   }
 
   preload () {
-    // WebFont.load({
-    //   google: {
-    //     families: ['Bangers']
-    //   },
-    //   active: this.fontsLoaded
-    // })
-
     let text = this.add.text(this.world.centerX, this.world.centerY, 'loading...', {
       font: '16px Arial',
       fill: '#dddddd',
@@ -88,9 +49,7 @@ export default class extends Phaser.State {
   }
 
   render () {
-    // if (this.fontsReady) {
     this.state.start('Splash')
-    // }
   }
 
   fontsLoaded () {
