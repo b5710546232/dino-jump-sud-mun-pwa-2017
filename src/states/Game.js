d2b17d56dec0092f6d10b526c587bbf7dd850ca4
Benjapol Worakan <benwrk@live.com>
2017-06-25 05:59:36 +0300

    Fix another typo

--- src/states/Game.js
+++ src/states/Game.js
@@ -219,7 +219,7 @@ export default class extends Phaser.State {
           this.top3Score = 'TOP 3 HIGH SCORE'
           let rank = 1
           scores.splice(0, 3).forEach((item) => {
-            this.top3Score += '\n' + `RANK ${rank}: ${item} score`
+            this.top3Score += '\n' + `RANK ${rank}: ${item}`
             rank++
           })
           this.top3Score += '\n' + `YOUR HIGH SCORE RANK IS ${yourRank}`
